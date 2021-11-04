c2bf5f1415495fd7bc6362fec08a97db393bc7b2
Joon Kang <joonkang@Joons-MBP-2.olin.edu>
2021-11-03 21:52:38 -0400

    integrated ramen dispenser

--- ramen_code/ramen_code.ino
+++ ramen_code/ramen_code.ino
@@ -6,6 +6,7 @@
 
 Servo powerservo;  // hotplate power servo
 Servo boilservo;  // hotplate boil button servo
+Servo ramen_servo;
 
 int incomingByte = 0; // for incoming serial data
 
@@ -13,9 +14,11 @@ int incomingByte = 0; // for incoming serial data
 int pos = 0;    // variable to store the servo position
 uint16_t power_pos = 90;
 uint16_t boil_pos = 270; // for the second button presser because the servo is oriented differently
+uint16_t ramen_default_pos = 0;
 
 uint8_t power_servo_pin = 9;
 uint8_t boil_servo_pin = 10;
+uint8_t ramen_servo_pin = 11;
 
 bool power = false;
 
@@ -27,8 +30,11 @@ void setup() {
   Serial.begin(9600);
   powerservo.attach(power_servo_pin);
   boilservo.attach(boil_servo_pin);
+  ramen_servo.attach(ramen_servo_pin);
+  
   powerservo.write(pos);
   boilservo.write(boil_pos);
+  ramen_servo.write(ramen_default_pos);
   sensors.begin();
 }
 
@@ -69,6 +75,14 @@ float temp_check(){
   return celcius;
 }
 
+int reactToInput(int input){
+  panServo.write(90 - input);
+  //0 is home position, 90 is arm extended and knocks the ramen out.
+  //We can probably play around with speeds here to make the ramen fall
+  //in a way that minimizes splash but that might not be v1
+  return input;
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
@@ -88,6 +102,10 @@ void loop() {
   Serial.println();
   Serial.println(power);
   if (temp > 95 && power){
+    reactToInput(80);
+    sleep(500);
+    reactToInput(0);
+    sleep(180000);
     hotplate_on_off();
   }
   
